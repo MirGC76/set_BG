431e2b71ff8b997abd4d914b5c3044eb15896582
MirGC76 <withjum@gmail.com>
2026-07-16 11:47:42 +0900

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,13 @@
+// Firebase 프로젝트 설정값을 여기에 붙여넣으세요.
+// Firebase 콘솔 > 프로젝트 설정 > 일반 > "내 앱" > 웹 앱(</>) > SDK 설정 및 구성 에서 확인할 수 있습니다.
+// (README.md의 "1. Firebase 프로젝트 만들기" 단계를 참고하세요)
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyDJjF6kWLl4lpRzctmr0rcHBRoLi2Cgj7E",
+  authDomain: "set-game-57b08.firebaseapp.com",
+  databaseURL: "https://set-game-57b08-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "set-game-57b08",
+  storageBucket: "set-game-57b08.firebasestorage.app",
+  messagingSenderId: "54122579677",
+  appId: "1:54122579677:web:43c540f792d12d8ee13d34"
+};
